1bf71f60fe3b0bc096c2cc80f3403812bad706be
Hebert <hebertdev82@gmail.com>
2025-06-23 10:58:50 -0300

    feat: construction page

---
+++ app/page.js
@@ -0,0 +1,20 @@
+import Counter from "./components/counter";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gradient-to-r from-black to-gray-900 text-white">
+      <div className="text-center space-y-6">
+        <h1 className="text-4xl md:text-6xl font-bold">
+          Nosso site está quase no ar!
+        </h1>
+        <p className="text-lg md:text-xl">Faltam apenas</p>
+
+        <Counter />
+
+        <p className="text-sm md:text-base">
+          Fique ligado! Estamos preparando algo incrível.
+        </p>
+      </div>
+    </div>
+  );
+}
